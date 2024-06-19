6a3427b3b194581128f110f37f4b1ad787638bd5
kutranyaroslav <kutranaroslav@gmail.com>
2024-06-19 12:15:38 +0200

    adding links

--- src/Controller/ModelsController.php
+++ src/Controller/ModelsController.php
@@ -2,7 +2,20 @@
 
 namespace App\Controller;
 
-class ModelsController
+use App\Repository\ModelsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+class ModelsController extends AbstractController
 {
+    #[Route('/models/{id<\d>}', name: 'app_models_show')]
+    public function show (int $id,ModelsRepository $modelsRepository ):Response
+    {
+        $model = $modelsRepository->find($id);
+        if(!$model){
+            throw $this->createNotFoundException('We do not have such model');
+        }
+        return $this->render('models/show.html.twig',['model'=>$model]);
+    }
 
 }
